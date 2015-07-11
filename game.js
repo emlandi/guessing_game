d1658f9be0a36c00a471f743d615945f2abf8319
Emily Landi <emily.landi@gmail.com>
2015-07-10 18:55:17 -0700

    made changes to html and js using DOM

---
+++ game.js
@@ -0,0 +1,51 @@
+  var Game = function(name, answer){
+  this.name = name;
+  this.answer = answer;
+
+  this.evaluateAnswer = function() {
+    var guess;
+
+    document.getElementById("intro").textContent = (this.name + " loves to travel. Can you guess how many countries " + this.name + " has visited?");
+    document.getElementById("intro").style.fontSize = "xx-large"
+
+    for (var i = 1; i <= 3; i++) {
+      guess = prompt("Guess " + i);
+      var guessEl = document.getElementById("guess");
+
+      if (guess == this.answer) {
+        guessEl.textContent = "Wow, good guess! You got it!";
+        guessEl.style.color = "green";
+        guessEl.align = "center";
+        guessEl.style.fontSize = "xx-large";
+
+        throw new Error("");
+
+      } else if (guess > this.answer) {
+        guessEl.textContent = "Not that many!";
+        guessEl.style.color = "red";
+        guessEl.align = "center";
+        guessEl.style.fontSize = "xx-large";
+
+      } else {
+        guessEl.textContent = "Nope! Guess higher next time.";
+        guessEl.style.color = "red";
+        guessEl.align = "center";
+        guessEl.style.fontSize = "xx-large";
+      }
+    }
+
+    guessEl.textContent = "The answer is " + this.answer + "."
+    guessEl.align = "center";
+    guessEl.style.fontSize = "xx-large";
+
+  };
+};
+
+//var gameEmily = new Game("Emily", 20);
+//gameEmily.evaluateAnswer();
+
+//var gameMario = new Game("Mario", 101);
+//gameMario.evaluateAnswer();
+
+var gameLuigi = new Game("Luigi", 87);
+gameLuigi.evaluateAnswer();
